a44c98af2baa50d3798a961c92c46b0b8f4af067
Akash Chatterjee <akashchatterjee@Akashs-MacBook-Air.local>
2021-06-27 21:55:21 +0530

    Added event emitter in todo-item-component

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/custom-classes/todo';
 
 @Component({
@@ -9,6 +9,7 @@ import { Todo } from 'src/app/custom-classes/todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo: Todo;
+  @Output() createTodo: EventEmitter<Todo> = new EventEmitter();
   constructor() { 
     this.todo = new Todo();
   }
